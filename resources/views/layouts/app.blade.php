a2f0447f956e307e15c3d50ab6da89acc2e72023
KautsarH <kautsarhasanah@gmail.com>
2020-12-16 00:07:24 +0800

    uncomment js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,7 +121,7 @@
     
             @yield('content')
      
-    <!-- <script src="{{ secure_asset('js/app.js') }}" defer async></script> -->
+    <script src="{{ secure_asset('js/app.js') }}" defer async></script>
     <script src="{{ secure_asset('js/script.js') }}" defer async></script>
 
     <script async defer src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
