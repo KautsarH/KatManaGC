a0cec8a45b58ee4bf684e1bb65ce86179b3fb13b
KautsarH <kautsarhasanah@gmail.com>
2021-01-04 22:59:46 +0800

    test logic

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -48,7 +48,7 @@ class PlannerController extends Controller
         $inactive = (array)$inactivedb;
         $stations = \App\Station::where('status', 'active')->orderBy('id')->get();
         $fstation = \App\Station::where('status', 'active')->orderBy('id')->first()->id;
-        $radius = 200;
+        $radius = 90;
         $locations = new Collection;
 
         $place = $request['place1'];
